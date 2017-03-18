7e25c7e8aaf10c1e87025f4eb46529721f206e56
sogloarcadius <rtsoglo@gmail.com>
2017-03-18 19:43:33 +0100

    dialog, feelings log or share,

--- app/src/main/java/presentation/FeelingsFragment.java
+++ app/src/main/java/presentation/FeelingsFragment.java
@@ -1,5 +1,6 @@
 package presentation;
 
+import android.app.Dialog;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -13,11 +14,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -34,6 +39,9 @@ public class FeelingsFragment extends Fragment {
 
     CustomGridAdapter customGridAdapter;
     GridView gridview;
+    Dialog dialog;
+
+    int _position;
 
     public FeelingsFragment() {
         // Empty constructor required for fragment subclasses
@@ -61,18 +69,8 @@ public class FeelingsFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        //update date
-
-/*        Calendar c = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss a");
-        String strDate = sdf.format(c.getTime());
-
-        TextView textView_date = (TextView) view.findViewById(R.id.feelings_date_now_id);
-        textView_date.setText(strDate);*/
-
-        // ==references to our images
-
-        Integer[] moodsImages = {
+        //references to our images
+        final Integer[] moodsImages = {
                 R.drawable.afraid,
                 R.drawable.angry,
                 R.drawable.bored,
@@ -143,64 +141,98 @@ public class FeelingsFragment extends Fragment {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
 
-//                MyDialogFragment dFragment = new MyDialogFragment();
-//                // Show DialogFragment
-//                dFragment.showDialog("dialog");
+                _position = position;
+                //custom dialog
+                dialog = new Dialog(getContext());
+
+                Calendar c = Calendar.getInstance();
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss a");
+                String strDate = sdf.format(c.getTime());
+
+                dialog.setContentView(R.layout.custom_dialog);
+                dialog.setTitle(strDate);
+
+                ImageView imageView = (ImageView) dialog.findViewById(R.id.dialogImageMood);
+                imageView.setImageResource(moodsImages[position]);
+
+                Button dialogButtonlog = (Button) dialog.findViewById(R.id.dialogButtonLog);
+                // if button is clicked, close the custom dialog
+                dialogButtonlog.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                        Toast.makeText(getContext(), getString(R.string.dialog_log_message), Toast.LENGTH_LONG).show();
+                    }
+                });
 
 
-                List<Intent> targetShareIntents = new ArrayList<Intent>();
-                Intent shareIntent = new Intent();
-                shareIntent.setAction(Intent.ACTION_SEND);
-                shareIntent.setType("text/plain");
-                PackageManager pm = getActivity().getPackageManager();
-                List<ResolveInfo> resInfos = pm.queryIntentActivities(shareIntent, 0);
-                if (!resInfos.isEmpty()) {
-                    System.out.println("Have package");
-                    for (ResolveInfo resInfo : resInfos) {
-                        String packageName = resInfo.activityInfo.packageName;
-
-                        if (packageName.contains("com.twitter.android") || packageName.contains("com.facebook.katana")
-                                || packageName.contains("com.whatsapp") || packageName.contains("com.google.android.apps.plus")
-                                || packageName.contains("com.google.android.talk") || packageName.contains("com.slack")
-                                || packageName.contains("com.google.android.gm") || packageName.contains("com.facebook.orca")
-                                || packageName.contains("com.yahoo.mobile") || packageName.contains("com.skype.raider")
-                                || packageName.contains("com.android.mms") || packageName.contains("com.linkedin.android")
-                                || packageName.contains("com.google.android.apps.messaging")) {
-                            Log.i("Package Name", packageName);
-                            Intent intent = new Intent();
-                            intent.setComponent(new ComponentName(packageName, resInfo.activityInfo.name));
+                Button dialogButtonlog_share = (Button) dialog.findViewById(R.id.dialogButtonLog_share);
+                // if button is clicked, close the custom dialog
+                dialogButtonlog_share.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                        Toast.makeText(getContext(), getString(R.string.dialog_log_message), Toast.LENGTH_SHORT).show();
 
 
-                            String text = "Shared from FeelShare ";
-                            Uri imageUri = Uri.parse("android.resource://fr.labsoglo.feelshare/drawable/" + _moods.get(position).getId());
-                            intent.putExtra("AppName", resInfo.loadLabel(pm).toString());
+                        List<Intent> targetShareIntents = new ArrayList<Intent>();
+                        Intent shareIntent = new Intent();
+                        shareIntent.setAction(Intent.ACTION_SEND);
+                        shareIntent.setType("text/plain");
+                        PackageManager pm = getActivity().getPackageManager();
+                        List<ResolveInfo> resInfos = pm.queryIntentActivities(shareIntent, 0);
+                        if (!resInfos.isEmpty()) {
+                            System.out.println("Have package");
+                            for (ResolveInfo resInfo : resInfos) {
+                                String packageName = resInfo.activityInfo.packageName;
 
-                            intent.setAction(Intent.ACTION_SEND);
-                            intent.putExtra(Intent.EXTRA_SUBJECT, text);
-                            intent.putExtra(Intent.EXTRA_TEXT, _moods.get(position).getDesc() + "\n\n" + "---\n" + "https://apps.soglomania.io/");
-                            intent.putExtra(Intent.EXTRA_STREAM, imageUri);
-                            intent.setType("image/*");
-                            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                            intent.setPackage(packageName);
-                            targetShareIntents.add(intent);
+                                if (packageName.contains("com.twitter.android") || packageName.contains("com.facebook.katana")
+                                        || packageName.contains("com.whatsapp") || packageName.contains("com.google.android.apps.plus")
+                                        || packageName.contains("com.google.android.talk") || packageName.contains("com.slack")
+                                        || packageName.contains("com.google.android.gm") || packageName.contains("com.facebook.orca")
+                                        || packageName.contains("com.yahoo.mobile") || packageName.contains("com.skype.raider")
+                                        || packageName.contains("com.android.mms") || packageName.contains("com.linkedin.android")
+                                        || packageName.contains("com.google.android.apps.messaging")) {
+                                    Log.i("Package Name", packageName);
+                                    Intent intent = new Intent();
+                                    intent.setComponent(new ComponentName(packageName, resInfo.activityInfo.name));
+
+
+                                    String text = "Shared from FeelShare ";
+                                    Uri imageUri = Uri.parse("android.resource://fr.labsoglo.feelshare/drawable/" + _moods.get(_position).getId());
+                                    intent.putExtra("AppName", resInfo.loadLabel(pm).toString());
+
+                                    intent.setAction(Intent.ACTION_SEND);
+                                    intent.putExtra(Intent.EXTRA_SUBJECT, text);
+                                    intent.putExtra(Intent.EXTRA_TEXT, _moods.get(_position).getDesc() + "\n\n" + "---\n" + "https://apps.soglomania.io/");
+                                    intent.putExtra(Intent.EXTRA_STREAM, imageUri);
+                                    intent.setType("image");
+                                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                                    intent.setPackage(packageName);
+                                    targetShareIntents.add(intent);
+                                }
+                            }
+                            if (!targetShareIntents.isEmpty()) {
+                                Collections.sort(targetShareIntents, new Comparator<Intent>() {
+                                    @Override
+                                    public int compare(Intent o1, Intent o2) {
+                                        return o1.getStringExtra("AppName").compareTo(o2.getStringExtra("AppName"));
+                                    }
+                                });
+                                Intent chooserIntent = Intent.createChooser(targetShareIntents.remove(0), "Select app to share");
+                                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, targetShareIntents.toArray(new Parcelable[]{}));
+                                startActivity(chooserIntent);
+                            } else {
+                                Toast.makeText(getActivity(), getString(R.string.no_app), Toast.LENGTH_LONG).show();
+                            }
                         }
                     }
-                    if (!targetShareIntents.isEmpty()) {
-                        Collections.sort(targetShareIntents, new Comparator<Intent>() {
-                            @Override
-                            public int compare(Intent o1, Intent o2) {
-                                return o1.getStringExtra("AppName").compareTo(o2.getStringExtra("AppName"));
-                            }
-                        });
-                        Intent chooserIntent = Intent.createChooser(targetShareIntents.remove(0), "Select app to share");
-                        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, targetShareIntents.toArray(new Parcelable[]{}));
-                        startActivity(chooserIntent);
-                    } else {
-                        Toast.makeText(getActivity(), "No app to share.", Toast.LENGTH_LONG).show();
-                    }
-                }
-//                Toast.makeText(getActivity(), _moods.get(position).getDesc() + position,
-//                        Toast.LENGTH_SHORT).show();
+                });
+
+                dialog.show();
+
+
+
             }
         });
 
